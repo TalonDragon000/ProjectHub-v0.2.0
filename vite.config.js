fabffd6c6b5d380b810af93f5c54de6b415459b3
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2026-05-18 17:14:16 -1000

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    { enforce: 'pre', ...mdx() },
-    react({ include: /\.(js|jsx|ts|tsx)$/ }),
-  ],
+  plugins: [react()],
 })
