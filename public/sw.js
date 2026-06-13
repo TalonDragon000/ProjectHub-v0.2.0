d7d8d46b670ac9d32628631383f1af8ef67cfcca
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2026-06-12 16:13:16 -1000

    Updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,10 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
+  if (e.request.method !== 'GET') || !e.request.url.startsWith('http')) {
+    console.log('Skipping non-http request', e.request.url);
+    } return;
+  };
 
   const url = new URL(e.request.url);
 
@@ -21,9 +24,11 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request)
       .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        return res;
+        if (res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+          return res;
       })
       .catch(() => caches.match(e.request))
   );
